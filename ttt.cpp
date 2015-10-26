89b217b9adef38a4ecd07a9e5ad998b429c640a9
rfordyce <rfordyce@users.noreply.github.com>
2015-10-26 14:07:54 -0500

    Many const pointer changes to functions, simplified rotation and symmetry board checks, speedup for game rotation and symmetry checks, rewrote testSymmetryRotationGame to be slightly more efficient .. make smaller commits

--- ttt.cpp
+++ ttt.cpp
@@ -8,29 +8,39 @@ bool b_cleanrotated = false;
 bool b_cleansymmetric = false;
 bool b_cleanboth = false;
 
-struct board {
-	// 0 1 2
-	// 3 4 5
-	// 6 7 8
+// 0 1 2
+// 3 4 5
+// 6 7 8
+class board
+{
 public:
+	board();
 	int layout[9];
-board() {
-	for (int i = 0; i < 9; i++) layout[i] = 0; //set all the entries to 0
-}
 };/**/
 
-class game { //class or struct?
+board::board()
+{
+	for (int i = 0; i < 9; i++) layout[i] = 0; //set all the entries to 0
+}
+
+class game {
 public:
+	game();
+	game(const game &g);
 	std::vector <board> boards;
 	int winner;
-	game() {
-		winner = 0; // no winner,1,2,draw
-	}
-	game(const game &g) {
-		boards = g.boards;
-		winner = g.winner;
-	}
 };/**/
+
+game::game()
+{
+	winner = 0; // no winner,1,2,draw
+}
+
+game::game(const game &g)
+{
+	boards = g.boards;
+	winner = g.winner;
+}
 
 const game templateGame;
 const board templateBoard;
@@ -49,7 +59,7 @@ void makeSeeds() //it is assumed that 1 moves first
 	for (int i = 0; i < 9; i++) (*games).at(i).boards.at(0).layout[i] = 1;
 }/**/
 
-int whoseturn(board b)
+int whoseturn(const board& b)
 {
 	int count = 0; //no moves on the board yet
 	for (int i = 0; i < 9; i++)
@@ -58,7 +68,7 @@ int whoseturn(board b)
 	return 2; //not turn 1
 }/**/
 
-int testWinner(board b)
+int testWinner(const board &b)
 {
 	// rows
 	switch(b.layout[0]*b.layout[1]*b.layout[2]) {
@@ -133,7 +143,7 @@ void addGames(game &g)
 	}
 }/**/
 
-void printBoard(board b)
+void printBoard(const board &b)
 {
 	std::cout << b.layout[0] << b.layout[1] << b.layout[2] << std::endl
 	          << b.layout[3] << b.layout[4] << b.layout[5] << std::endl
@@ -141,13 +151,13 @@ void printBoard(board b)
 	;
 }/**/
 
-void printGame(game g)
+void printGame(const game &g)
 {
 	for (int i = 0; i < (int) g.boards.size(); i++)
 		printBoard(g.boards.at(i));
 }/**/
 
-board rotate(board b, int n)
+/*board rotate(board b, int n) // FIXME: no need for this function anymore
 {
 	//if (n < 1) return b; //this safeguard is already performed by for loop
 	board b1,b2;
@@ -167,7 +177,21 @@ board rotate(board b, int n)
 	return b1; //b1 = b2
 }/**/
 
-bool testRotationBoard(board b, board btest, int rotations)
+void rotate2(board &b)
+{
+	board b2 = b; // ugly
+	//b.layout[4] = b.layout[4]; //center value [4] is equal
+	b.layout[0] = b2.layout[6]; // 0 1 2
+	b.layout[1] = b2.layout[3]; // 3 4 5
+	b.layout[2] = b2.layout[0]; // 6 7 8
+	b.layout[5] = b2.layout[1];
+	b.layout[8] = b2.layout[2];
+	b.layout[7] = b2.layout[5];
+	b.layout[6] = b2.layout[8];
+	b.layout[3] = b2.layout[7];
+}/**/
+
+/*bool testRotationBoard(board b, board btest, int rotations)
 {
 	board btemp = rotate(btest,rotations);
 	for (int i = 0 ; i < 9 ;i++) //check every layout entry
@@ -175,7 +199,52 @@ bool testRotationBoard(board b, board btest, int rotations)
 	return true; //all the entries matched, therefore the rotation is the same
 }/**/
 
-bool testRotationGame(game g, game gtest)
+bool testRotationBoard(const board& b, const board& btest, const int& rotations)
+{
+	//if (b.layout[4] != btest.layout[4]) return false; // test center
+	switch(rotations) {
+	case 1:
+		if (b.layout[0] != btest.layout[6]) return false; // 0 1 2
+		if (b.layout[1] != btest.layout[3]) return false; // 3 4 5
+		if (b.layout[2] != btest.layout[0]) return false; // 6 7 8
+		if (b.layout[3] != btest.layout[7]) return false;
+		if (b.layout[5] != btest.layout[1]) return false; // 6 3 0
+		if (b.layout[6] != btest.layout[8]) return false; // 7 4 1
+		if (b.layout[7] != btest.layout[5]) return false; // 8 5 2
+		if (b.layout[8] != btest.layout[6]) return false;
+		return true;
+		break;
+	case 2:
+		if (b.layout[0] != btest.layout[8]) return false; // 0 1 2
+		if (b.layout[1] != btest.layout[7]) return false; // 3 4 5
+		if (b.layout[2] != btest.layout[6]) return false; // 6 7 8
+		if (b.layout[3] != btest.layout[5]) return false;
+		if (b.layout[4] != btest.layout[4]) return false; // center
+		if (b.layout[5] != btest.layout[3]) return false; // 8 7 6 
+		if (b.layout[6] != btest.layout[2]) return false; // 5 4 3
+		if (b.layout[7] != btest.layout[1]) return false; // 2 1 0
+		if (b.layout[8] != btest.layout[0]) return false;
+		return true;
+		break;
+	case 3:
+		if (b.layout[0] != btest.layout[2]) return false; // 0 1 2
+		if (b.layout[1] != btest.layout[5]) return false; // 3 4 5
+		if (b.layout[2] != btest.layout[8]) return false; // 6 7 8
+		if (b.layout[3] != btest.layout[1]) return false;
+		if (b.layout[4] != btest.layout[4]) return false; // center
+		if (b.layout[5] != btest.layout[7]) return false; // 2 5 8
+		if (b.layout[6] != btest.layout[0]) return false; // 1 4 7
+		if (b.layout[7] != btest.layout[3]) return false; // 0 3 6
+		if (b.layout[8] != btest.layout[6]) return false;
+		return true;
+		break;
+	default:
+		// flag error - this this should always be 1,2,3
+		return true;
+	}
+}
+
+bool testRotationGame(const game &g, const game &gtest)
 {
 	if (g.boards.size() != gtest.boards.size()) return false; //cannot be the same if not the same size
 	if (g.winner != gtest.winner) return false; //cannot be the same if different winner
@@ -184,7 +253,9 @@ bool testRotationGame(game g, game gtest)
 	for (int rotation = 1; rotation <= 3 ; rotation++) { //rotations 1,2,3
 		for (int boardindex = 0; boardindex < (int) g.boards.size() ; boardindex++) { //all boards to be rotated by rotation
 			if (testRotationBoard(g.boards.at(boardindex), gtest.boards.at(boardindex), rotation))
-				counter_layout_entries++; //better to kick this out if not true
+				counter_layout_entries++;
+			else
+				boardindex = 99; // exit for()
 		}
 		if ((int) g.boards.size() == counter_layout_entries) return true; //this rotation is the same
 		counter_layout_entries = 0; //reset counter for next rotation
@@ -192,7 +263,7 @@ bool testRotationGame(game g, game gtest)
 	return false;
 }/**/
 
-board symmetrize(board b, int n) //complete
+/*board symmetrize(board b, int n) // FIXME: no need for this function anymore
 {
 	board b2;
 	switch (n) {
@@ -246,7 +317,7 @@ board symmetrize(board b, int n) //complete
 	return b2;
 }/**/
 
-bool testSymmetric(board b, board btest, int symmetryindex)
+/*bool testSymmetric(board b, board btest, int symmetryindex)
 {
 	board b2;
 	b2 = symmetrize(btest,symmetryindex);
@@ -255,7 +326,64 @@ bool testSymmetric(board b, board btest, int symmetryindex)
 	return true;
 }/**/
 
-bool testSymmetryGame(game g, game gtest)
+bool testSymmetric(const board& b, const board& btest, const int &symmetryindex)
+{
+	switch (symmetryindex) {
+	case 1: //147
+		if(b.layout[0] != btest.layout[2]) return false; // 0 1 2
+		if(b.layout[1] != btest.layout[1]) return false; // 3 4 5
+		if(b.layout[2] != btest.layout[0]) return false; // 6 7 8
+		if(b.layout[3] != btest.layout[5]) return false;
+		if(b.layout[4] != btest.layout[4]) return false;
+		if(b.layout[5] != btest.layout[3]) return false; // 2 1 0
+		if(b.layout[6] != btest.layout[8]) return false; // 5 4 3
+		if(b.layout[7] != btest.layout[7]) return false; // 8 7 6
+		if(b.layout[8] != btest.layout[6]) return false;
+		return true;
+		break;
+	case 2: //048
+		if(b.layout[0] != btest.layout[0]) return false; // 0 1 2
+		if(b.layout[1] != btest.layout[3]) return false; // 3 4 5
+		if(b.layout[2] != btest.layout[6]) return false; // 6 7 8
+		if(b.layout[3] != btest.layout[1]) return false;
+		if(b.layout[4] != btest.layout[4]) return false;
+		if(b.layout[5] != btest.layout[7]) return false; // 0 3 6
+		if(b.layout[6] != btest.layout[2]) return false; // 1 4 7
+		if(b.layout[7] != btest.layout[5]) return false; // 2 5 8
+		if(b.layout[8] != btest.layout[8]) return false;
+		return true;
+		break;
+	case 3: //246
+		if(b.layout[0] != btest.layout[8]) return false; // 0 1 2
+		if(b.layout[1] != btest.layout[5]) return false; // 3 4 5
+		if(b.layout[2] != btest.layout[2]) return false; // 6 7 8
+		if(b.layout[3] != btest.layout[7]) return false;
+		if(b.layout[4] != btest.layout[4]) return false;
+		if(b.layout[5] != btest.layout[1]) return false; // 8 5 2
+		if(b.layout[6] != btest.layout[6]) return false; // 7 4 1
+		if(b.layout[7] != btest.layout[3]) return false; // 6 3 0
+		if(b.layout[8] != btest.layout[0]) return false;
+		return true;
+		break;
+	case 4: //345
+		if(b.layout[0] != btest.layout[6]) return false; // 0 1 2
+		if(b.layout[1] != btest.layout[7]) return false; // 3 4 5
+		if(b.layout[2] != btest.layout[8]) return false; // 6 7 8
+		if(b.layout[3] != btest.layout[3]) return false;
+		if(b.layout[4] != btest.layout[4]) return false;
+		if(b.layout[5] != btest.layout[5]) return false; // 6 7 8
+		if(b.layout[6] != btest.layout[0]) return false; // 3 4 5
+		if(b.layout[7] != btest.layout[1]) return false; // 0 1 2
+		if(b.layout[8] != btest.layout[2]) return false;
+		return true;
+		break;
+	default:
+		// flag error - this this should always be 1,2,3,4
+		return true;
+	}
+}
+
+bool testSymmetryGame(const game& g, const game& gtest)
 {
 	if (g.boards.size() != gtest.boards.size()) return false; //cannot be the same if not the same size
 	if (g.winner != gtest.winner) return false; //cannot be the same if different winner
@@ -264,6 +392,8 @@ bool testSymmetryGame(game g, game gtest)
 		for (int boardindex = 0; boardindex < (int) g.boards.size(); boardindex++) {
 			if (testSymmetric(g.boards.at(boardindex),gtest.boards.at(boardindex),symmetryindex))
 				counter_symmetric_entries++;
+			else
+				boardindex = 99; // exit for()
 		}
 		if ((int) g.boards.size() == counter_symmetric_entries) return true;
 		counter_symmetric_entries = 0;
@@ -271,21 +401,22 @@ bool testSymmetryGame(game g, game gtest)
 	return false;
 }/**/
 
-bool testSymmetryRotationGame(game g, game gtest)
+bool testSymmetryRotationGame(const game& g, const game& gtest)
 {
 	if (g.boards.size() != gtest.boards.size()) return false; //cannot be the same if not the same size
 	if (g.winner != gtest.winner) return false; //cannot be the same if different winner
 	int counter_symmetric_entries = 0;
 
-	game gmodified;
+	//game gmodified;
+	game gmodified = gtest;
 	
 	for (int symmetryindex = 1; symmetryindex <= 4; symmetryindex++) { //test symmetry 1,2,3,4
-		gmodified = gtest;
+		//gmodified = gtest;
 
-		//modify gmodified to be a rotation
 		for (int rotation = 1; rotation <= 3 ; rotation++) { //rotations 1,2,3
 			for (int boardindex = 0; boardindex < (int) g.boards.size() ; boardindex++) { //rotate all the boards
-				gmodified.boards.at(boardindex) = rotate(gtest.boards.at(boardindex),rotation);
+				//gmodified.boards.at(boardindex) = rotate(gtest.boards.at(boardindex),rotation);
+				rotate2(gmodified.boards.at(boardindex));
 			}
 			if (testSymmetryGame(g,gmodified)) return true; //just use testSymmetryGame to check it
 		} // next rotation
